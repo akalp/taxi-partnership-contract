c6cf6d0b9442418d4e57be99ef47e0192a80a268
Hasan Akalp <hasan.akalp@gmail.com>
2019-11-29 00:20:08 +0300

    less functional interface

--- src/js/app.js
+++ src/js/app.js
@@ -38,38 +38,112 @@ App = {
   },
 
   bindEvents: function () {
-    $(document).on('click', '.btn-join', App.handleJoin);
+    $(document).on('click', '#btn-join', App.handleJoin);
   },
 
-  getGlobals: async function (adopters, account) {
+  getGlobals: async function () {
     var taxiPartnershipInstance = await App.contracts.TaxiPartnership.deployed();
+
     await taxiPartnershipInstance.manager.call().then(manager => {
-      $(document).find('#manager').text(manager);
+      $('#manager').text(manager);
     }).catch(function (err) {
       console.log(err.message);
     });
 
-    web3.eth.getBalance(taxiPartnershipInstance.address, (err, balance) => {
-      balance = web3.fromWei(balance, "ether") + " ETH";
-      $(document).find('#total-ether').text(balance);
+    await taxiPartnershipInstance.ownedCar.call().then(async carId => {
+      if (carId == 0) $('#owned-car').text("There is no car.");
+      else {
+        $('#owned-car').text("Car ID: " + carId);
+        await taxiPartnershipInstance.lastExpenseTime.call().then(time => {
+          var time = new Date(1000 * lpd);
+          time = new Date(time.setMonth(time.getMonth() + 6));
+          $('#next-expense').text("Next expense time: " + time);
+        });
+      }
     });
 
+    web3.eth.getBalance(taxiPartnershipInstance.address, (err, balance) => {
+      if (err) {
+        console.log(err);
+      } else {
+        balance = web3.fromWei(balance, "ether") + " ETH";
+        $('#total-ether').text(balance);
+      }
+    });
+
+    // fill dividend time
+    await taxiPartnershipInstance.lastProfitDist.call().then(lpd => {
+      var time = new Date(1000 * lpd);
+      time = new Date(time.setMonth(time.getMonth() + 6));
+      $('#next-dividend').text(time);
+    });
+
+    // fill participants
     await taxiPartnershipInstance.getParticipantCount.call().then(async count => {
-      $(document).find('#participants').empty();
+      $('#participants').empty();
       for (var i = 0; i < count; i++) {
         var participant = await taxiPartnershipInstance.participantAccts.call(i)
-        $(document).find('#participants').append('<div>' + participant + '</div>');
+        $('#participants').append('<div>' + participant + '</div>');
       }
 
+      // Check according account type
       web3.eth.getAccounts(async function (error, accounts) {
         if (error) {
           console.log(error);
         }
         var account = accounts[0];
+
+        // if accout participant
         await taxiPartnershipInstance.participantCheck.call(account).then(async bool => {
-        $(document).find('#user-ether').text(bool ? web3.fromWei(await taxiPartnershipInstance.balances.call(account), 'ether') + " ETH":"You are not a participant.");
-        $(document).find('.btn-join')[0].disabled = bool;
-      });
+          $('#user-ether').text(bool ? web3.fromWei(await taxiPartnershipInstance.balances.call(account), 'ether') + " ETH" : "You are not a participant.");
+          $('#btn-join').disabled = bool;
+          if (bool) {
+            $('#btn-join').text('Joined!');
+
+            await taxiPartnershipInstance.carForSale.call().then(test => {
+              if (test[0] != 0 && test[2] != 0 && new Date(test[2]) < Date.now()) {
+                $('#carbuy-proposal').show();
+                $('#carbuy-id').text(test[0]);
+                $('#carbuy-price').text(web3.fromWei(test[1], 'ether') + " ETH");
+                $('#carbuy-time').text("Offer valid till: " + new Date(test[2]));
+                $('#btn-buycar-vote').show();
+              }
+            });
+            await taxiPartnershipInstance.carForRepurchase.call().then(test => {
+              if (test[0] != 0 && test[2] != 0 && new Date(test[2]) < Date.now()) {
+                $('#carsell-proposal').show();
+                $('#carsell-id').text(test[0]);
+                $('#carsell-price').text(web3.fromWei(test[1], 'ether') + " ETH");
+                $('#carsell-time').text("Offer valid till: " + new Date(test[2]));
+                $('#btn-sellcar-vote').show();
+              }
+            });
+          } else {
+            await taxiPartnershipInstance.participationFee.call().then(fee => {
+              fee = web3.fromWei(fee, 'ether') + " ETH";
+              $('#join-price').text(fee);
+            }).catch(function (err) {
+              console.log(err.message);
+            });
+          }
+        });
+
+        // if account manager
+        if (account === await taxiPartnershipInstance.manager.call()) {
+          console.log(1)
+          await taxiPartnershipInstance.carForSale.call().then(test => {
+            if (test[0] != 0 && new Date(test[2]) < Date.now())
+              $('#btn-buycar').show();
+            $('#btn-buycar').text("Vote: " + test[3]);
+          });
+          await taxiPartnershipInstance.carForRepurchase.call().then(test => {
+            if (test[0] != 0 && new Date(test[2]) < Date.now())
+              $('#btn-sellcar').show();
+            $('#btn-sellcar').text("Vote: " + test[3]);
+          });
+        }
+
+
       });
     }).catch(function (err) {
       console.log(err.message);
@@ -102,7 +176,7 @@ App = {
 };
 
 $(function () {
-  $(window).load(function () {
+  $(window).on('load', function () {
     App.init();
     window.ethereum.on('accountsChanged', function () {
       App.getGlobals();
